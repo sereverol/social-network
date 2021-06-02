73556e2204cf00a44f0c752640496e7cf2f6f367
Silfrido Reverol <silfridoer@gmail.com>
2021-06-02 00:19:36 -0400

    database connected!

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.use(express.json({ extended: false }));
 app.use(cors());
